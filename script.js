a5640c1dd9b1036d126a4427654defd0e026413a
andrelara2002 <andrelara2002@gmail.com>
2020-11-26 13:24:09 -0300

    1 file modified

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const buttonElement = document.getElementById("buttonClear");
 const inputElement = document.getElementById("inputElement");
 const labelElement = document.getElementById("resultLabel");
 
-inputElement.addEventListener('input', event => {
-
-  let value = inputElement.value
+inputElement.addEventListener("input", event => {
+  let value = inputElement.value;
   value = value.toString();
 
-  if (value.includes("2") || value.includes("3") || value.includes('4') || value.includes('5')  || value.includes('6')  || value.includes('7')  || value.includes('8')  || value.includes('9')) {
-    labelElement.innerHTML = "Precisa ser Binário";
+  if (
+    value.includes("2") ||
+    value.includes("3") ||
+    value.includes("4") ||
+    value.includes("5") ||
+    value.includes("6") ||
+    value.includes("7") ||
+    value.includes("8") ||
+    value.includes("9")
+  ) {
+    value = parseInt(value, 10);
+    value = value.toString(2);
+    labelElement.innerHTML = value + " bin";
   } else {
     try {
       value = parseInt(value, 2);
@@ -16,15 +26,16 @@ inputElement.addEventListener('input', event => {
         labelElement.innerHTML = "Insira um número";
       } else {
         labelElement.innerHTML = value;
+        labelElement.innerHTML = value + " dec";
       }
     } catch {
       alert("Infelizmente não foi possível converter");
     }
   }
-})
+});
 
 clearWorkspace = () => {
-  labelElement.innerHTML = 'Digite um binário'
-  inputElement.value = ''
-  inputElement.focus = '';
-}
+  labelElement.innerHTML = "Digite um binário";
+  inputElement.value = "";
+  inputElement.focus = "";
+};
