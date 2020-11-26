97c67220f93af9f55029e16ba597f623fb824baf
andrelara2002 <andrelara2002@gmail.com>
2020-11-26 00:14:42 -0300

    3 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello!')
+const buttonElement = document.getElementById("buttonClear");
+const inputElement = document.getElementById("inputValue");
+const labelElement = document.getElementById("resultLabel");
+
+executeConversion = () => {
+  let value = inputElement.value;
+  value = value.toString();
+  console.log(value);
+
+  if (value.includes("1") || value.includes("0")) {
+    try {
+      value = parseInt(value, 2);
+      if (isNaN(value)) {
+        labelElement.innerHTML = "Insira um número";
+      } else {
+        labelElement.innerHTML = value;
+      }
+    } catch {
+      alert("Infelizmente não foi possível converter");
+    }
+  } else {
+    labelElement.innerHTML = "Precisa ser Binário";
+  }
+};
